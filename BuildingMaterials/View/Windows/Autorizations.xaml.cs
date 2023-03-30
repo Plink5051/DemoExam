c3a5c88b8104da947ca2d29fc7734b5d23ec8fae
= <=>
2023-03-30 16:57:57 +0300

    BuildingMaterial

---
+++ BuildingMaterials/View/Windows/Autorizations.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BuildingMaterials.Autorizatoins
+{
+    /// <summary>
+    /// авторизация, если обычный пользователь, то попадает в магазин. если админ - в магазин с админ панелью
+    /// 
+    /// </summary>
+    public partial class Autorizations : Page
+    {
+        public Autorizations()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BuildingMaterials/View/Windows/HiddenShop.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BuildingMaterials
+{
+    /// <summary>
+    /// Машгазин с админ панелью
+    /// </summary>
+    public partial class HiddenShop : Page
+    {
+        public HiddenShop()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
